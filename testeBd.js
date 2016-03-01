96969743016f958ea8ada81cbdc8ced6fdcb4cdc
Raphaell <rafaelk-f@hotmail.com>
2016-03-01 16:50:07 -0300

    .

--- testeBd.js
+++ testeBd.js
@@ -1,4 +1,15 @@
 var logger = require('./lib/logger.js');
 var bd     = require('./lib/bd.js');
 
-bd.login('751374');
+//bd.login('751374');
+
+
+var meuObj = {
+    'id_modulo'         : '16307',
+    'id_veiculo'        : dados.id_veiculo,
+    'datahora'          : dados.datahora,
+    'longitude'         : dados.longitude,
+    'velocidade'        : dados.velocidade,
+    'datahora_gravacao' : dados.datahora_gravacao,
+    'panico'            : dados.panico
+};
