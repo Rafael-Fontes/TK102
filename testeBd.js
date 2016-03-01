34659d037efd2dec8491d393c4216c0a8414eae2
Raphaell <rafaelk-f@hotmail.com>
2016-03-01 17:19:45 -0300

    .

--- testeBd.js
+++ testeBd.js
@@ -6,10 +6,13 @@ var bd     = require('./lib/bd.js');
 
 var meuObj = {
     'id_modulo'         : '16307',
-    'id_veiculo'        : dados.id_veiculo,
-    'datahora'          : dados.datahora,
-    'longitude'         : dados.longitude,
-    'velocidade'        : dados.velocidade,
-    'datahora_gravacao' : dados.datahora_gravacao,
-    'panico'            : dados.panico
+    //'id_veiculo'        : dados.id_veiculo,
+    'datahora'          : '2016-03-01',
+    'latitude'          : '1111111111',
+    'longitude'         : '1111111111',
+    'velocidade'        : '300',
+    'datahora_gravacao' : '2016-03-01 16:53:00',
+    'panico'            : '1'
 };
+
+bd.salvar(meuObj);
