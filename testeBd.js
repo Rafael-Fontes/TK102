e67127ff65f805a115404853b7de53c63cee3b22
Rafael Fontes <rafaelk-f@hotmail.com>
2016-03-02 00:37:37 -0300

    .

--- testeBd.js
+++ testeBd.js
@@ -1,18 +1,18 @@
 var logger = require('./lib/logger.js');
 var bd     = require('./lib/bd.js');
 
-//bd.login('751374');
+bd.login('751374');
 
 
 var meuObj = {
     'id_modulo'         : '16307',
-    //'id_veiculo'        : dados.id_veiculo,
+    'id_veiculo'        : null,
     'datahora'          : '2016-03-01',
-    'latitude'          : '1111111111',
-    'longitude'         : '1111111111',
+    'latitude'          : '-15.8961000000000',
+    'longitude'         : '-48.1178000000000',
     'velocidade'        : '300',
     'datahora_gravacao' : '2016-03-01 16:53:00',
     'panico'            : '1'
 };
 
-bd.salvar(meuObj);
+//bd.salvar(meuObj);
